e436c8932624a8a0fbcaf00a2117a7e38c9a7b11
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-08 11:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+let table = document.getElementById('myTable');
+
 function createTable() {
     //Write your code here
-  
+	let rowValue = prompt("Input number of rows");
+	let colValue = prompt("Input number of columns");
+
+	rowValue = parseInt(rowValue);
+	colValue = parseInt(colValue);
+	
+	// if(rowValue !== Number && colValue !== Number){
+	// 	alert('please enter a valid number')
+	// 	return;
+	// }
+
+	
+	if(rowValue < 0 && colValue < 0){
+		alert('please enter a positive number')
+		return;
+	}
+	for(let i = 0 ; i<rowValue ; i++){
+		let newRow = table.insertRow(i);
+		for(let j=0 ; j<colValue ; j++){
+			
+			let cell = newRow.insertCell(j);
+			cell.innerHTML = `Row-${i} Column-${j}`;
+		}
+	}
+	
 }
